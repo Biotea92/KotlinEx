cff55cb42e6030695aa81f8cd72c68467b25ed99
kimwoojong <skekdnwhd@gmail.com>
2024-01-08 16:20:45 +0900

    위임

---
+++ src/main/kotlin/delegate/DelegateAttribute2.kt
@@ -0,0 +1,18 @@
+package org.example.delegate
+
+class LazySample {
+    init {
+        println("created!")            // 1
+    }
+
+    val lazyStr: String by lazy {
+        println("계산!")          // 2
+        "지연 값"
+    }
+}
+
+fun main() {
+    val sample = LazySample()         // 1
+    println("lazyStr = ${sample.lazyStr}")  // 2
+    println(" = ${sample.lazyStr}")  // 3
+}
